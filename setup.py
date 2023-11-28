aaab45030fca18fdb31cf3c3ce6de07a0a943ce0
crrao <31522080+crrao@users.noreply.github.com>
2023-11-28 13:38:30 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup 
+  
+setup( 
+    name='backlog_network', 
+    version='0.1', 
+    description='A sample Python package', 
+    author='John Doe', 
+    author_email='rao.chiragr@gmail.com', 
+    packages=['my_package'], 
+    install_requires=[ 
+        'numpy', 
+        'pandas', 
+        'math',
+        
+    ], 
+) 
