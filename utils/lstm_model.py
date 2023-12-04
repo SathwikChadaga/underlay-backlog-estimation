f28ca3cd77e4e891cfdfc3edebdde5ee929735c0
SathwikChadaga <chadagasathwik@gmail.com>
2023-12-03 19:44:10 -0500

    added hyperparameter tuning

--- utils/lstm_model.py
+++ utils/lstm_model.py
@@ -2,15 +2,20 @@ import torch
 import torch.nn as nn
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_sizes, output_size):
         super(LSTMModel, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.hidden_layers = nn.ModuleList()
+        self.hidden_layers.append(nn.LSTM(input_size, hidden_sizes[0], num_layers=1, batch_first=True))
+        for ii in range(1, len(hidden_sizes)):
+            self.hidden_layers.append(nn.LSTM(hidden_sizes[ii-1], hidden_sizes[ii], num_layers=1, batch_first=True))
+        self.output_layer = nn.Linear(hidden_sizes[-1], output_size)
+
 
     def forward(self, x):
-        lstm_out, _ = self.lstm(x)
-        output = self.fc(lstm_out[:, -1, :])  
-        return output
+        for layer in self.hidden_layers:
+            x, _ = layer(x)
+        x = self.output_layer(x[:, -1, :]) 
+        return x
 
     def evaluate(self, x):
         with torch.no_grad():
